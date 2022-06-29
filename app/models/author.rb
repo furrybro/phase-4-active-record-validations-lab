c71fcc7e2d76a003503e30737c3d520ca2ca364d
jeena park <jeenapark15@gmail.com>
2022-06-29 11:08:04 -0400

    commit tree 9566baab568260104fcd0f89bb3de26200f22064

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: { case_senstive: false }
+    validates :phone_number, length: { is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :must_have_these_words
+
+  
+
+    def must_have_these_words
+        title_array = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+        if title_array.none? { |word| word.match title }
+            errors.add(:title, "Must include a clickbait word!")
+        end
+    end
 end
